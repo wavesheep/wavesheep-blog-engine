6565c138b25aa8e0fa350a9f1e706b279a8a043b
wavesheep <1196046520@qq.com>
2020-07-28 09:40:25 +0800

    feat: add post error layout

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="h-screen w-screen relative bg-cover bg-center"
+    :style="{background: 'url(' + img + ')'}"
+  >
+    <Nav mode="dark" />
+    <div class="absolute inset-0 m-auto h-32 w-full text-center text-white-full">
+      <h1 class="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">{{error.statusCode}}</h1>
+      <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">{{description}}</h2>
+    </div>
+    <Footer mode="light" class="absolute bottom-0 w-full text-center" />
+  </div>
+</template>
+
+<script>
+import BLOGCONFIG from "@/data/blog.config";
+
+export default {
+  layout: "blank",
+  props: ["error"],
+  data() {
+    return {
+      img: "",
+      description: "",
+    };
+  },
+  created() {
+    this.img = BLOGCONFIG.error.img;
+    this.description = BLOGCONFIG.error.description;
+  },
+};
+</script>
